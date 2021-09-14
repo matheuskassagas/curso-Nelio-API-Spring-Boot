bd3883c0a2010f686b62b053ae1e74cae6ad2440
Matheus Gigliotti <gigliotti_@hotmail.com>
2021-09-14 15:55:43 -0300

    Create project

---
+++ src/main/java/com/cursonelio/javaspringboot/cursoNelio/CursoNelioApplication.java
@@ -0,0 +1,13 @@
+package com.cursonelio.javaspringboot.cursoNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursonelio/javaspringboot/cursoNelio/CursoNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursonelio.javaspringboot.cursoNelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
