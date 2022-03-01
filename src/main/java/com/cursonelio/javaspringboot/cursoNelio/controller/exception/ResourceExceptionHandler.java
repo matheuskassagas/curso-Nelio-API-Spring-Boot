fc6b6fb194159d762bd70deb2bba0d3fd85f65ef
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-03-01 16:49:10 -0300

    personalizando o retorno da validação

--- src/main/java/com/cursonelio/javaspringboot/cursoNelio/controller/exception/ResourceExceptionHandler.java
+++ src/main/java/com/cursonelio/javaspringboot/cursoNelio/controller/exception/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package com.cursonelio.javaspringboot.cursoNelio.controller.exception;
 import com.cursonelio.javaspringboot.cursoNelio.service.exception.DataIntegrityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import com.cursonelio.javaspringboot.cursoNelio.service.exception.ObjectNotFounfException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,4 +25,16 @@ public class ResourceExceptionHandler {
         StandardError standardError = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+        ValidationError standardError = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
+
+        for (FieldError x : e.getBindingResult().getFieldErrors()){
+            standardError.addError(x.getField(), x.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
+    }
+
 }
