ef4528db287b93076d37c0d3518b9961e34e26c0
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-03-03 16:54:23 -0300

    total e subtotal de um pedido

--- src/main/java/com/cursonelio/javaspringboot/cursoNelio/repository/entity/ItemPedido.java
+++ src/main/java/com/cursonelio/javaspringboot/cursoNelio/repository/entity/ItemPedido.java
@@ -28,6 +28,11 @@ public class ItemPedido implements Serializable {
         this.quantidade = quantidade;
         this.preco = preco;
     }
+
+    public double getSubTotal(){
+        return (preco - desconto) * quantidade;
+    }
+
     @JsonIgnore
     public Pedido getPedido(){
         return id.getPedido();
@@ -68,6 +73,7 @@ public class ItemPedido implements Serializable {
     public void setPreco(Double preco) {
         this.preco = preco;
     }
+
 
     @Override
     public boolean equals(Object o) {
