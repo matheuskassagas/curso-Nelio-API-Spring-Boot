86bc314b54abf6bdec0800b64dcdc71cb7313189
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-03-03 14:15:27 -0300

    validacao de cpf e cnpj com annotation customizado

--- src/main/java/com/cursonelio/javaspringboot/cursoNelio/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/cursonelio/javaspringboot/cursoNelio/service/validation/ClienteInsertValidator.java
@@ -2,14 +2,20 @@ package com.cursonelio.javaspringboot.cursoNelio.service.validation;
 
 import com.cursonelio.javaspringboot.cursoNelio.controller.exception.FieldMessage;
 import com.cursonelio.javaspringboot.cursoNelio.dto.Request.ClienteRequestNew;
+import com.cursonelio.javaspringboot.cursoNelio.repository.ClienteRepository;
+import com.cursonelio.javaspringboot.cursoNelio.repository.entity.Cliente;
 import com.cursonelio.javaspringboot.cursoNelio.repository.entity.enuns.TipoCliente;
 import com.cursonelio.javaspringboot.cursoNelio.service.validation.util.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteRequestNew> {
+
+    @Autowired
+    private ClienteRepository repository;
 
     @Override
     public void initialize(ClienteInsert ann) {
@@ -20,13 +26,17 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         List<FieldMessage> list = new ArrayList<>();
 
         if(objDto.getTipoCliente().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())){
-            list.add(new FieldMessage("cpfOuCnpj", "CPF invalido"));
+            list.add(new FieldMessage("CPF", "CPF invalido"));
         }
 
         if(objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
-            list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
+            list.add(new FieldMessage("CNPJ", "CNPJ invalido"));
         }
-        // inclua os testes aqui, inserindo erros na lista
+
+        Cliente aux = repository.findByEmail(objDto.getEmail());
+        if (aux != null){
+            list.add(new FieldMessage("email", "Email ja existente"));
+        }
 
         for (FieldMessage e : list) {
             context.disableDefaultConstraintViolation();
