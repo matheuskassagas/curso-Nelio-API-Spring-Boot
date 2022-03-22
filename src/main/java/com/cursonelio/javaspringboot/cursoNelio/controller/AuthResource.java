ac80d5976d1b982c7dd9443859fc44555672473a
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-03-21 21:16:24 -0300

    Refresh Token

---
+++ src/main/java/com/cursonelio/javaspringboot/cursoNelio/controller/AuthResource.java
@@ -0,0 +1,31 @@
+package com.cursonelio.javaspringboot.cursoNelio.controller;
+
+import com.cursonelio.javaspringboot.cursoNelio.security.JWTUtil;
+import com.cursonelio.javaspringboot.cursoNelio.security.UserSS;
+import com.cursonelio.javaspringboot.cursoNelio.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response){
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+}
+
