d516213f685dd45b7123136d3cf96a6d4e62a7f7
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-03-22 21:59:21 -0300

    Configurando S3

--- src/main/java/com/cursonelio/javaspringboot/cursoNelio/CursoNelioApplication.java
+++ src/main/java/com/cursonelio/javaspringboot/cursoNelio/CursoNelioApplication.java
@@ -4,6 +4,7 @@ import com.cursonelio.javaspringboot.cursoNelio.repository.entity.*;
 import com.cursonelio.javaspringboot.cursoNelio.repository.entity.enuns.EstadoPagamento;
 import com.cursonelio.javaspringboot.cursoNelio.repository.entity.enuns.TipoCliente;
 import com.cursonelio.javaspringboot.cursoNelio.repository.*;
+import com.cursonelio.javaspringboot.cursoNelio.service.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,13 +17,16 @@ import java.util.Arrays;
 @SpringBootApplication
 public class CursoNelioApplication implements CommandLineRunner { //permiti implementar um metodo auxiliar
 
+	@Autowired
+	private S3Service service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursoNelioApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		service.uploadFile("C:\\temp\\Capturar.PNG");
 
 	}
 }
