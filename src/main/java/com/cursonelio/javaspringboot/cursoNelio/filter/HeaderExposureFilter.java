a2e33a703da9e65259348c1fb70a6ead4626868b
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-03-29 17:01:35 -0300

    Header expose

---
+++ src/main/java/com/cursonelio/javaspringboot/cursoNelio/filter/HeaderExposureFilter.java
@@ -0,0 +1,25 @@
+package com.cursonelio.javaspringboot.cursoNelio.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class HeaderExposureFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        res.addHeader("access-control-expose-headers", "Authorization");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
